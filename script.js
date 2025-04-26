9f14430ffb48a90774269698d2b06b1974c8a216
Adil Hussain <silver@Adils-MacBook-Air.local>
2025-04-26 13:17:06 +0530

    nav updated in all page

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-
-
-	// Partner Slider JS
-	$('.partner-slider').owlCarousel({
-		loop: true,
-		margin: 24,
-		nav: false,
-		dots: false,
-		autoplay: true,
-		smartSpeed: 1000,
-		autoplayHoverPause: true,
-		responsive: {
-			0: {
-				items: 2,
-			},
-			414: {
-				items: 2,
-			},
-			576: {
-				items: 3,
-			},
-			768: {
-				items: 4,
-			},
-			992: {
-				items: 5,
-			},
-			1200: {
-				items: 6,
-			},
-		},
-	});
+document
+  .querySelector(".nav-bar-toggle")
+  .addEventListener("click", function () {
+    document.querySelector(".navbar-nav").classList.toggle("show");
+  });
+// Partner Slider JS
+$(".partner-slider").owlCarousel({
+  loop: true,
+  margin: 24,
+  nav: false,
+  dots: false,
+  autoplay: true,
+  smartSpeed: 1000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    414: {
+      items: 2,
+    },
+    576: {
+      items: 3,
+    },
+    768: {
+      items: 4,
+    },
+    992: {
+      items: 5,
+    },
+    1200: {
+      items: 6,
+    },
+  },
+});
